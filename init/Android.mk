060e81ec4dec2949989aa224b18a564a090fda65
klozz <shaclone707@gmail.com>
2019-02-11 02:11:40 -0500

    ghost:libinit: fix libinit for lineage remove nfc_enhaced
    Change-Id: I08c0b243fc3ba8c4d1acae466fce7004aa411cdc
    Signed-off-by: klozz <shaclone707@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -3,8 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_CFLAGS := -Wall
+LOCAL_STATIC_LIBRARIES := libbase libselinux
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_ghost.cpp
 LOCAL_MODULE := libinit_ghost
